87d921986f380ac0df20351415b8e90fca9ef2d8
WaleedHarfany <138225263+WaleedHarfany@users.noreply.github.com>
2023-06-30 16:38:24 +0300

    Add files via upload

---
+++ src/Components/Data.js
@@ -0,0 +1,202 @@
+
+/* types and items data saved here */
+
+const Types = [
+    {
+        id: 1,
+        itemId: "hat",
+        name: "Hats",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/HatType.png?alt=media&token=a91f5e7f-1582-4b1d-b6f5-76c8afa583c3"
+    },
+    {
+        id: 2,
+        itemId: "jacket",
+        name: "Jackets",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/Jacket.png?alt=media&token=a1697dbd-778d-41d3-a980-06ef57148ed3"
+    },
+    {
+        id: 3,
+        itemId: "pant",
+        name: "Pants",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/PantsType.png?alt=media&token=a0c53599-8caa-49ae-91cc-408db93fdc69"
+    },
+    {
+        id: 4,
+        itemId: "boot",
+        name: "Boots",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/BootsType.png?alt=media&token=70d2ba54-eb5a-4128-9048-540c190f3cea"
+    },
+    {
+        id: 5,
+        itemId: "bandanna",
+        name: "Bandanna",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/Bandanna.png?alt=media&token=066dc2c1-a8f6-4cff-93dc-440cca5095e7"
+    },
+    {
+        id: 6,
+        itemId: "rope",
+        name: "Ropes",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/Rope.png?alt=media&token=cfc35321-3c71-45a9-ae58-5e5a6aeadd16"
+    },
+];
+
+const Items = [
+    {
+        id: 1,
+        itemId: "hat",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/hat1.png?alt=media&token=9fc26cc1-ae7e-49ad-90d2-d775515806f2",
+        name: "Straw Hat",
+        price: "50",
+        description: "Classic straw cowboy's hat",
+    },
+    {
+        id: 2,
+        itemId: "hat",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/hat2.png?alt=media&token=1a0bb848-7cbf-4c88-a6f1-9b9004cb3364",
+        name: "Large Hat",
+        price: "50",
+        description: "Large cowboy's hat",
+    },
+    {
+        id: 3,
+        itemId: "hat",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/hat3.png?alt=media&token=ed8ba53f-e345-4c8b-b86f-b945950ed319",
+        name: "Felt Hat",
+        price: "40",
+        description: "Felt styled hat",
+    },
+    {
+        id: 4,
+        itemId: "hat",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/hat4.png?alt=media&token=530a65c2-2a0d-4b7a-9cef-9c0106a157bb",
+        name: "Black Hat",
+        price: "30",
+        description: "Black cowboy's hat",
+    },
+    {
+        id: 5,
+        itemId: "hat",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/hat5.png?alt=media&token=921e17d8-e3f5-4281-a303-52a5ed70df8c",
+        name: "Holiday Hat",
+        price: "20",
+        description: "Holiday styled hat",
+    },
+    {
+        id: 6,
+        itemId: "hat",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/hat6.png?alt=media&token=c0774d72-12f5-42dd-90be-1025911c351e",
+        name: "Chief Hat",
+        price: "40",
+        description: "Chief styled hat ",
+    },
+    /*__________________________________________________________*/
+    {
+        id: 7,
+        itemId: "jacket",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/Jacket.png?alt=media&token=a1697dbd-778d-41d3-a980-06ef57148ed3",
+        name: "Jacket",
+        price: "150",
+        description: "Cowboy's jacket made with same cloth ropes materials that the real cowboys used.",
+    },
+    /*__________________________________________________________*/
+    {
+        id: 8,
+        itemId: "pant",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/PantsType.png?alt=media&token=a0c53599-8caa-49ae-91cc-408db93fdc69",
+        name: "Pant",
+        price: "100",
+        description: "Jeans made with same cloth ropes materials that the real cowboys used.",
+    },
+    /*__________________________________________________________*/
+    {
+        id: 9,
+        itemId: "boot",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/boot1.png?alt=media&token=5c557994-3b3b-49d3-820a-c70ea40bbb9e",
+        name: "Classic Boots",
+        price: "50",
+        description: "Classic cowboy's boots",
+    },
+    {
+        id: 10,
+        itemId: "boot",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/boot2.png?alt=media&token=32671fd1-9dbd-48cc-b06f-41bc4fb233fd",
+        name: "Spurs Boots",
+        price: "50",
+        description: "Well designed coloeres boots",
+    },
+    {
+        id: 11,
+        itemId: "boot",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/boot3.png?alt=media&token=ce8dcf6a-fd05-4681-be2d-e6f62c3d337c",
+        name: "Snakeskin Boots",
+        price: "50",
+        description: "Cowboy's boots made with real snake skin.",
+    },
+    {
+        id: 12,
+        itemId: "boot",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/boot4.png?alt=media&token=b7a4a4ad-31f4-42ab-9be3-9ca3ec7852f2",
+        name: "Vintage Boots",
+        price: "40",
+        description: "Well designed boots that orange striped.",
+    },
+    {
+        id: 13,
+        itemId: "boot",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/boot5.png?alt=media&token=83982502-146a-4ef1-913a-672b315babf4",
+        name: "Stetson Boots",
+        price: "30",
+        description: "women's style cowboy boots.",
+    },
+    {
+        id: 14,
+        itemId: "boot",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/boot6.png?alt=media&token=9de7a975-4174-4a31-a939-3bbd36d75b92",
+        name: "Blue Boots",
+        price: "20",
+        description: "Well designed boots that blue striped.",
+    },
+    /*__________________________________________________________*/
+    {
+        id: 15,
+        itemId: "bandanna",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/Bandanna.png?alt=media&token=066dc2c1-a8f6-4cff-93dc-440cca5095e7",
+        name: "Bandanna",
+        price: "5",
+        description: "Bandanna made with same cloth ropes materials that the real cowboys used.",
+    },
+    /*__________________________________________________________*/
+    {
+        id: 16,
+        itemId: "rope",
+        imgSrc:
+            "https://firebasestorage.googleapis.com/v0/b/cowboys-shop-75b1d.appspot.com/o/Rope.png?alt=media&token=cfc35321-3c71-45a9-ae58-5e5a6aeadd16",
+        name: "Rope",
+        price: "10",
+        description: "10M rope made with the same materials that the real cowboys used.",
+    },
+    /*__________________________________________________________*/
+];
+
+export { Types, Items };
